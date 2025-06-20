03dd3cb880f6c56828b4f4f50b35b4d1b0363d41
xzy231132 <killiangreen@gmail.com>
2025-06-19 20:54:11 -0400

    Update newjavascript.js

--- newjavascript.js
+++ newjavascript.js
@@ -1,6 +1,9 @@
+// This function is used to store topics and hides other topics when a subject is clicked.
+
 function selectTopics(subjectId) {
   const topics = document.querySelectorAll('.topics');
   topics.forEach(div => {
+    // I had to use the === operator for validation purposes when using netbeans for this, as netbeans did not enjoy my usage of ==.
     if (div.id === `${subjectId}-topics`) {
         if (div.style.display === 'block') {
             div.style.display = 'none';
